333d99e8145b4704aa9908c64708f74d0e5db20c
cnnrbrn <connorbobrien@gmail.com>
2020-08-09 16:50:17 +0200

    Add answers

--- js/products.js
+++ js/products.js
@@ -14,3 +14,28 @@ const products = [
         soldOut: true,
     },
 ];
+
+const container = document.querySelector(".container");
+
+let html = "";
+
+for (let i = 0; i < products.length; i++) {
+    let displayPrice = "Unavailable";
+
+    if (products[i].price) {
+        displayPrice = products[i].price;
+    }
+
+    let colour = "green";
+
+    if (products[i].soldOut) {
+        colour = "red";
+    }
+
+    html += `<div class="product">
+                <h4 style="color: ${colour}">${products[i].name}</h4>
+                <div>Price: ${displayPrice}</div>
+            </div>`;
+}
+
+container.innerHTML = html;
