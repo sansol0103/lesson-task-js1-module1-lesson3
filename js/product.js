2f40c88cf6e8075f6e3db990a2b7bf1afab17cd2
sansol0103 <sansol_01@pm.me>
2023-01-25 12:20:46 +0100

    Did the lesson

--- js/product.js
+++ js/product.js
@@ -3,3 +3,22 @@ const product = {
     price: 99,
     onSale: true,
 };
+
+const container = document.querySelector(".container");
+
+let displayPrice = "Unavailable";
+
+if (product.price) {
+    displayPrice = product.price;
+}
+
+let decoration = "none";
+
+if (product.onSale) {
+    decoration = "underline";
+}
+
+container.innerHTML = `<div class="product">
+                        <h4 style="text-decoration: ${decoration}">${product.name}</h4>
+                        <div>Price: ${displayPrice}</div>
+                    </div>`;
--- js/products.js
+++ js/products.js
@@ -14,3 +14,28 @@ const products = [
         soldOut: true,
     },
 ];
+
+const container = document.querySelector(".container");
+
+let html = "";
+
+for (let i = 0; i < products.length; i++) {
+    let displayPrice ="Unavailable";
+
+    if (products[i].price) {
+        displayPrice = products[i].price;
+    }
+
+    let color = "green"; 
+
+    if(products[i].soldOut) {
+        color ="red";
+    }
+
+    html += `<div class="product">
+                <h4 style="color: ${color}">${products[i].name}</h4>
+                <div>Price: ${displayPrice}</div>
+            </div>`;
+}
+
+container.innerHTML = html;
